d3ae98189ddb869252b10ce5f40b18992aa432bd
kapiten-orca <idhamsetia45@gmail.com>
2023-01-07 18:02:03 +0700

    laravel9-crud

---
+++ app/Http/Controllers/mahasiswaController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\mahasiswa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class mahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = mahasiswa::orderBy('nim', 'desc')->paginate(5);
+        return view('mahasiswa.index')-> with('data', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Session::flash('nim', $request->nim);
+        Session::flash('nama', $request->nama);
+        Session::flash('jurusan', $request->jurusan);
+        $request->validate([
+            'nim' => 'required|numeric|unique:mahasiswa,nim',
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ],
+        [
+            'nim.required'=> 'Nim wajib diisi!',
+            'nim.numeric'=> 'Nim wajib dalam angka!',
+            'nim.unique'=> 'Nim yang diisikan sudah ada dalam database!',
+            'nama.required'=> 'Nama wajib diisi!',
+            'jurusan.required'=> 'Jurusan wajib diisi!',
+    ]);
+        $data = [
+            'nim'=> $request->nim,
+            'nama'=> $request->nama,
+            'jurusan'=> $request->jurusan,
+        ];
+        mahasiswa::create($data);
+        return redirect()->to('mahasiswa')->with('success','Berhasil menambahkan data');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
